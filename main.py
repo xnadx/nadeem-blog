0cb84a25d7caa770ba72a0afd1cfe3270aa5cd69
Nadeem Ahmed <vanadeem@gmail.com>
2021-08-17 00:07:16 +0530

    change sqlite to postgres add gravtar in main

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ from dotenv import load_dotenv
 
 load_dotenv("D:/DevOps/EnvironmentVariables/.env")
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("nadeem_blog_secret_app_key")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
+gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
+
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
