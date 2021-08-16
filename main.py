ee823ec1c705a4d111c54daa7cb5b4c44dd3a63b
Nadeem Ahmed <vanadeem@gmail.com>
2021-08-16 23:26:08 +0530

    Add gunicorn

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
+import os
+from dotenv import load_dotenv
 
+load_dotenv("D:/DevOps/EnvironmentVariables/.env")
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("nadeem_blog_secret_app_key")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
